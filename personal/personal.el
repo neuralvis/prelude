58e5c02f0af722d0c7ba9ebf12959425d41796e8
Madhu Srinivasan <madhu.srinivasan@kaust.edu.sa>
2013-12-12 08:20:52 +0300

    minor syntax fix

--- personal/personal.el
+++ personal/personal.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  (disable-theme 'zenburn)
-  (load-theme 'solarized-dark t)
+ '(disable-theme 'zenburn)
+ '(load-theme 'solarized-dark t)
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 (custom-set-faces

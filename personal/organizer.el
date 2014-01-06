dcbf4e189fe6b3b42a53abec065bacb52b4216e2
Madhu Srinivasan <smadhueagle@outlook.com>
2014-01-06 16:12:37 +0300

    planning scripts

---
+++ personal/organizer.el
@@ -0,0 +1,8 @@
+(setq org-todo-keywords
+ '((sequence
+    "TODO(t)"  ; next action
+    "TOBLOG(b)"  ; next action
+    "STARTED(s)"
+    "WAITING(w@/!)"
+    "POSTPONED(p)" "SOMEDAY(s@/!)" "|" "DONE(x!)" "CANCELLED(c@)")
+   (sequence "TODELEGATE(-)" "DELEGATED(d)" "COMPLETE(x)")))

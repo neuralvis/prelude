79df49dc0ce294f4d8bebd6456370b5923af3fca
Madhu Srinivasan <madhu.srinivasan@kaust.edu.sa>
2014-05-07 20:41:08 +0300

    added multiple cursor mode

--- personal/custom.el
+++ personal/custom.el
@@ -12,10 +12,13 @@
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3")
  '(global-linum-mode t)
- '(linum-format "%6d\u25B8 "))
+ '(linum-format "%6d\u25B8 ")
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(prelude-require-packages '(multiple-cursors projectile org glsl-mode))

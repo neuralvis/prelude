a9245626a8a989a55aa17a50a3fd66cebbb4297a
Madhu Srinivasan <smadhueagle@outlook.com>
2014-08-14 15:18:56 +0300

    added ecb source path, and path to custom ecb install

--- personal/ecb-init.el
+++ personal/ecb-init.el
@@ -1,6 +1,11 @@
+;;; specify the package path
+
+(add-to-list 'load-path "../../.ecb")
+
 ;;; activate ecb
 (require 'ecb)
 (require 'ecb-autoloads)
 
 (setq ecb-layout-name "left13")
 (setq ecb-show-sources-in-directories-buffer 'always)
+(setq ecb-source-path '("~/develop"))

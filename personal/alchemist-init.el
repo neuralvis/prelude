bf53bedb4dc621a84ac2519d214275bc5a3b9a60
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2016-04-30 07:48:55 +0300

    updates to theming and packages

--- personal/alchemist-init.el
+++ personal/alchemist-init.el
@@ -2,11 +2,23 @@
 
 ;;; Code:
 
+(require 'company)
 (require 'alchemist)
+(require 'rust-mode)
+(require 'racer)
+
 (setq alchemist-mix-command "mix")
 (setq alchemist-iex-program-name "iex")
 (setq alchemist-execute-command "elixir")
 (setq alchemist-compile-command "elixirc")
 (add-hook 'after-init-hook 'global-company-mode)
+
+;;Taken from https://github.com/racer-rust/emacs-racer
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(global-set-key (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
 (provide 'alchemist-init)
 ;;; alchemist-init.el ends here
--- personal/custom.el
+++ personal/custom.el
@@ -3,17 +3,45 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(ecb-layout-window-sizes (quote (("devel" (ecb-directories-buffer-name 0.2184873949579832 . 0.7323943661971831) (ecb-history-buffer-name 0.2184873949579832 . 0.2535211267605634) (ecb-methods-buffer-name 0.19747899159663865 . 0.9859154929577465)))))
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(ecb-layout-window-sizes
+   (quote
+    (("devel"
+      (ecb-directories-buffer-name 0.2184873949579832 . 0.7323943661971831)
+      (ecb-history-buffer-name 0.2184873949579832 . 0.2535211267605634)
+      (ecb-methods-buffer-name 0.19747899159663865 . 0.9859154929577465)))))
  '(ecb-options-version "2.40")
  '(fci-rule-color "#383838")
  '(global-linum-mode t)
  '(global-visual-line-mode 1)
  '(linum-format "%6d▸ ")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(prelude-guru nil)
  '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

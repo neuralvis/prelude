4b6e7f0036fb69c9fff38bc135731db0efc88d30
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2016-06-12 09:48:40 +0300

    ecb default dirs

--- personal/ecb-init.el
+++ personal/ecb-init.el
@@ -18,6 +18,4 @@
 (setq ecb-layout-name "devel")
 (setq ecb-show-sources-in-directories-buffer 'always)
 (setq ecb-source-path '("~/develop/"
-                        "~/develop/vyslexia/garuda/CMakeExternals"
-                        "~/develop/garuda/CMakeExternals"
-                        "/home/vyslexia/develop/vyslexia/garuda"))
+                        "/var/remote/projects/k1129/turbulentflamevis"))

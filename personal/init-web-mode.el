4235b7fae1aa3971b812892a778b71066a0943ab
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2017-03-20 09:17:34 +0300

    setup indentation modes

--- personal/init-web-mode.el
+++ personal/init-web-mode.el
@@ -12,12 +12,29 @@
 (add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
+
+
+(defun my-setup-indent (n)
+  ;; java/c/c++
+  (setq-local c-basic-offset n)
+  ;; web development
+  (setq-local coffee-tab-width n) ; coffeescript
+  (setq-local javascript-indent-level n) ; javascript-mode
+  (setq-local js-indent-level n) ; js-mode
+  (setq-local js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq-local web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq-local web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq-local web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq-local css-indent-offset n) ; css-mode
+  )
+
+(setq indent-tabs-mode nil)
+;; indent 2 spaces width
+(my-setup-indent 2)
 
 (setq web-mode-enable-auto-pairing t)
 (setq web-mode-enable-current-element-highlight t)
+
 
 (provide 'web-mode)
 

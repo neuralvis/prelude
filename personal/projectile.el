5255ff4d5fab7da49cc717ad1f8f8d7655c98b43
Madhu Srinivasan <msrinivasa@cray.com>
2020-11-29 00:40:32 -0800

    added ivy mode, removed ecb-init

--- personal/projectile.el
+++ personal/projectile.el
@@ -1,3 +1,17 @@
 ;; Projectile options
 
+
+(straight-use-package 'ivy)
+(straight-use-package 'counsel)
+(straight-use-package 'swiper)
+(straight-use-package 'projectile)
+
+
+;; configuration set from
+;; https://docs.projectile.mx/projectile/configuration.html
 (setq projectile-indexing-method 'alien)
+(setq projectile-completion-system 'ivy)
+
+;; sort files by recently active buffers
+;; and then recently opened files
+(setq projectile-sort-order 'recently-active)

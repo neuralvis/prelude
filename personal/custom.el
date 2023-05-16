93976ca0b5664de9dad1a2ef110da8487a2291b8
Madhu Srinivasan <madsrini@amd.com>
2023-05-16 09:44:19 -0500

    setup highlight mode

--- personal/custom.el
+++ personal/custom.el
@@ -60,9 +60,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(highlight ((t (:background "color-234" :foreground "brightwhite")))))
 
-(prelude-require-packages '(multiple-cursors projectile org glsl-mode ))
+;;(prelude-require-packages '(multiple-cursors projectile org glsl-mode ))
 
 (provide 'custom)
 

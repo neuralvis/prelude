a765a82a205e1a5cabc92638cea13fcd3c367596
Madhu Srinivasan <madsrini@amd.com>
2023-05-16 09:46:08 -0500

    revamp rust mode

--- personal/rust.el
+++ personal/rust.el
@@ -1,10 +1,67 @@
-(straight-use-package 'rust-mode)
-(straight-use-package 'racer)
+(straight-use-package 'rustic)
+(straight-use-package 'lsp-mode)
+(straight-use-package 'lsp-ui)
 (straight-use-package 'smartparens)
+(straight-use-package 'yasnippet)
+
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
+
+(use-package lsp-mode
+             :ensure
+             :commands lsp
+             :custom
+             ;; what to use when checking on-save. "check" is default, I prefer clippy
+             (lsp-rust-analyzer-cargo-watch-command "clippy")
+             (lsp-eldoc-render-all t)
+             (lsp-idle-delay 0.6)
+             ;; enable / disable the hints as you prefer:
+             (lsp-rust-analyzer-server-display-inlay-hints t)
+             (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+             (lsp-rust-analyzer-display-chaining-hints t)
+             (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+             (lsp-rust-analyzer-display-closure-return-type-hints t)
+             (lsp-rust-analyzer-display-parameter-hints nil)
+             (lsp-rust-analyzer-display-reborrow-hints nil)
+             :config
+             (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+             :ensure
+             :commands lsp-ui-mode
+             :custom
+             (lsp-ui-peek-always-show t)
+             (lsp-ui-sideline-show-hover t)
+             (lsp-ui-doc-enable nil))
 
 ;;setting up rust racer
-(require 'rust-mode)
-(require 'racer)
 (require 'smartparens)
 
 ;; Indetations are spaces
@@ -12,29 +69,35 @@
           (lambda () (setq indent-tabs-mode nil)))
 
 ;; run rust-fmt on save
-;; (setq rust-format-on-save t)
-
-;; setting path for racer
-;;Taken from https://github.com/racer-rust/emacs-racer
-(setq racer-cmd "~/.cargo/bin/racer")
-(setq racer-rust-src-path
-      (concat (string-trim
-               (shell-command-to-string "rustc --print sysroot"))
-              "/lib/rustlib/src/rust/library"))
-
-;; start racer and eldoc mode
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
+(setq rust-format-on-save t)
 
 ;; Always start smartparens mode in rust-mode.
 (add-hook 'rust-mode #'smartparens-mode)
 
 ;; company mode
-(add-hook 'racer-mode-hook #'company-mode)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-(setq company-tooltip-align-annotations t)
+;;(add-hook 'racer-mode-hook #'company-mode)
+;;(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+;;(setq company-tooltip-align-annotations t)
+(use-package company
+             :ensure
+             :custom
+             (company-idle-delay 0.5) ;; how long to wait until popup
+             ;; (company-begin-commands nil) ;; uncomment to disable popup
+             :bind
+             (:map company-active-map
+	                 ("C-n". company-select-next)
+	                 ("C-p". company-select-previous)
+	                 ("M-<". company-select-first)
+	                 ("M->". company-select-last)))
+
+(use-package yasnippet
+             :ensure
+             :config
+             (yas-reload-all)
+             (add-hook 'prog-mode-hook 'yas-minor-mode)
+             (add-hook 'text-mode-hook 'yas-minor-mode))
 
 ;; bind compile run keys
-(define-key rust-mode-map (kbd "C-c C-c") 'rust-compile)
-(define-key rust-mode-map (kbd "C-c C-r") 'rust-run)
+;;(define-key rust-mode-map (kbd "C-c C-c") 'rust-compile)
+;;(define-key rust-mode-map (kbd "C-c C-r") 'rust-run)
 

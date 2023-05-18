2cbf19d7181962f69afbedc7b8872b7887f6dd60
Madhu Srinivasan <madsrini@amd.com>
2023-05-17 20:58:39 -0500

    a working rustic impl

--- personal/rust.el
+++ personal/rust.el
@@ -2,12 +2,23 @@
 (straight-use-package 'rustic)
 (straight-use-package 'lsp-mode)
 (straight-use-package 'lsp-ui)
+(straight-use-package 'company)
+(straight-use-package 'company-lsp)
 (straight-use-package 'smartparens)
 (straight-use-package 'yasnippet)
 (straight-use-package 'flycheck)
+(straight-use-package 'rust-playground)
+(straight-use-package 'toml-mode)
 
 
-(use-package flycheck :ensure)
+
+;; When using this directly, you will need to have use-package installed:
+;; M-x package-install, select use-package. But if you start via
+;; `standalone.el', this is being taken care of automatically.
+
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; rustic = basic rust-mode + additions
 
 (use-package rustic
   :ensure
@@ -19,7 +30,10 @@
               ("C-c C-c r" . lsp-rename)
               ("C-c C-c q" . lsp-workspace-restart)
               ("C-c C-c Q" . lsp-workspace-shutdown)
-              ("C-c C-c s" . lsp-rust-analyzer-status))
+              ("C-c C-c s" . lsp-rust-analyzer-status)
+              ("C-c C-c e" . lsp-rust-analyzer-expand-macro)
+              ("C-c C-c d" . dap-hydra)
+              ("C-c C-c h" . lsp-ui-doc-glance))
   :config
   ;; uncomment for less flashiness
   ;; (setq lsp-eldoc-hook nil)
@@ -27,7 +41,6 @@
   ;; (setq lsp-signature-auto-activate nil)
 
   ;; comment to disable rustfmt on save
-  (setq rustic-format-on-save t)
   (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
 
 (defun rk/rustic-mode-hook ()
@@ -39,70 +52,102 @@
     (setq-local buffer-save-without-query t))
   (add-hook 'before-save-hook 'lsp-format-buffer nil t))
 
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; for rust-analyzer integration
+
 (use-package lsp-mode
-             :ensure
-             :commands lsp
-             :custom
-             ;; what to use when checking on-save. "check" is default, I prefer clippy
-             (lsp-rust-analyzer-cargo-watch-command "clippy")
-             (lsp-eldoc-render-all t)
-             (lsp-idle-delay 0.6)
-             ;; enable / disable the hints as you prefer:
-             (lsp-rust-analyzer-server-display-inlay-hints t)
-             (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
-             (lsp-rust-analyzer-display-chaining-hints t)
-             (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
-             (lsp-rust-analyzer-display-closure-return-type-hints t)
-             (lsp-rust-analyzer-display-parameter-hints nil)
-             (lsp-rust-analyzer-display-reborrow-hints nil)
-             :config
-             (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; This controls the overlays that display type and other hints inline. Enable
+  ;; / disable as you prefer. Well require a `lsp-workspace-restart' to have an
+  ;; effect on open projects.
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
 (use-package lsp-ui
-             :ensure
-             :commands lsp-ui-mode
-             :custom
-             (lsp-ui-peek-always-show t)
-             (lsp-ui-sideline-show-hover t)
-             (lsp-ui-doc-enable nil))
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
 
 
-;; Always start smartparens mode in rust-mode.
-(require 'smartparens)
-(add-hook 'rust-mode #'smartparens-mode)
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; inline errors
+
+(use-package flycheck :ensure)
 
 
-;; Indetations are spaces
-(add-hook 'rust-mode-hook
-          (lambda () (setq indent-tabs-mode nil)))
-
-;; run rust-fmt on save
-(setq rust-format-on-save t)
-
-
-;; company mode
-;;(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-;;(setq company-tooltip-align-annotations t)
-(use-package company
-             :ensure
-             :custom
-             (company-idle-delay 0.5) ;; how long to wait until popup
-             ;; (company-begin-commands nil) ;; uncomment to disable popup
-             :bind
-             (:map company-active-map
-	                 ("C-n". company-select-next)
-	                 ("C-p". company-select-previous)
-	                 ("M-<". company-select-first)
-	                 ("M->". company-select-last)))
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; auto-completion and code snippets
 
 (use-package yasnippet
-             :ensure
-             :config
-             (yas-reload-all)
-             (add-hook 'prog-mode-hook 'yas-minor-mode)
-             (add-hook 'text-mode-hook 'yas-minor-mode))
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
 
-;; bind compile run keys
-;;(define-key rust-mode-map (kbd "C-c C-c") 'rust-compile)
-;;(define-key rust-mode-map (kbd "C-c C-r") 'rust-run)
+(use-package company
+  :ensure
+  :bind
+  (:map company-active-map
+              ("C-n". company-select-next)
+              ("C-p". company-select-previous)
+              ("M-<". company-select-first)
+              ("M->". company-select-last))
+  (:map company-mode-map
+        ("<tab>". tab-indent-or-complete)
+        ("TAB". tab-indent-or-complete)))
 
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (or (do-yas-expand)
+      (company-complete-common)))
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "::") t nil)))))
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
+
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; Create / cleanup rust scratch projects quickly
+
+(use-package rust-playground :ensure)
+
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; for Cargo.toml and other config files
+
+(use-package toml-mode :ensure)

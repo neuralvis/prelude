57895b32bafd4a268acdbe17ef7129e5d893b5ff
Madhu Srinivasan <madhu.srinivasan@outlook.com>
2024-01-28 00:28:39 -0600

    workgroups added

---
+++ personal/workgroups.el
@@ -0,0 +1,12 @@
+(straight-use-package 'workgroups2)
+
+;;(setq wg-session-load-on-start t)    ; default: (not (daemonp))
+
+;; Change prefix key (before activating WG)
+(setq wg-prefix-key (kbd "C-c z"))
+
+;; Change workgroups session file
+(setq wg-session-file "~/.emacs.d/.emacs_workgroups")
+
+(workgroups-mode 1)
+
